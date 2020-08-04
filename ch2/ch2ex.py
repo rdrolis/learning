8e9d7acc7cf1e16dc9f58cae78ae30de0e62cda7
Raymond Rolison <rolisrgti06@gmail.com>
2020-08-04 17:17:30 -0500

    First two chapter exrcises

--- ch2/ch2ex.py
+++ ch2/ch2ex.py
@@ -5,9 +5,9 @@ Created on Tue Aug  4 10:16:15 2020
 
 @author: rdrolis
 """
-
+#Exercise 2-1
 message = "This is a python message."
 print(message)
-
+#Exercise 2-2
 message = "Python is kind of fun."
 print(message)
--- ch2/full_name.py
+++ ch2/full_name.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
 first_name = "ava"
-last_name = "devine"
+last_name = "johnson"
 full_name =f"{first_name} {last_name}"
 print(full_name.title())
-
+message = f"Hello, {full_name.title()}"
+print (message)
+print()
+#Adding tabs to string
+print("Python")
+print("\tPython")
+print()
+#Add newline to string
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+print()
+#Stripping Whitespace
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
 
