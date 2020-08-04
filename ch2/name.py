50077e047608a3e1f9f02e2b538ea489786527e7
Ray Rolison <rolisrgti06@gmail.com>
2020-08-04 11:38:55 -0500

    2

---
+++ ch2/name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 11:03:28 2020
+
+@author: rdrolis
+"""
+
+name = "ada lovelace"
+print(name.title())
+print()
+print(name.upper())
+print(name.lower())
