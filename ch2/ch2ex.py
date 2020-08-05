e01e391e8d53ac35b6433ff384d2b35cacd1a469
Ray Rolison <rolisrgti06@gmail.com>
2020-08-04 19:37:42 -0500

    chapter exercises 3-7

--- ch2/ch2ex.py
+++ ch2/ch2ex.py
@@ -8,6 +8,33 @@ Created on Tue Aug  4 10:16:15 2020
 #Exercise 2-1
 message = "This is a python message."
 print(message)
+print()
 #Exercise 2-2
 message = "Python is kind of fun."
 print(message)
+print()
+#Exercise 2-3
+name = "shim yi young"
+message = f"{name.title()} is my favorite actress!"
+print(message)
+print()
+#Exercise 2-4
+print(name.lower())
+print(name.upper())
+print(name.title())
+print()
+#Exercise 2-5
+message = f"Oscar Wilde once said, 'Be yourself. Everyone else is already taken.'"
+print(message)
+print()
+#Exercise 2-6
+famous_person = "oscar wilde"
+quote = f"{famous_person.title()} once said, 'Be yourself. Everyone else is already taken."
+print(quote)
+print()
+#Exercise 2-7
+actress = " Actress:\n\t Shim Yi Young "
+print(actress.lstrip())
+print(actress.rstrip())
+print(actress.strip())
+print()
