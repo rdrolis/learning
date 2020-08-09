32444597ddeca30ede1dac068808f4a8d2401684
Ray Rolison <rolisrgti06@gmail.com>
2020-08-09 15:08:32 -0500

    more ch 5

--- examples/toppings.py
+++ examples/toppings.py
@@ -9,3 +9,20 @@ Created on Sat Aug  8 12:43:23 2020
 r_top = 'mushrooms'
 if r_top != 'anchovies':
     print("Hold the Anchovies!")
+# Doesn't work doesn't test all conditions
+r_top = ['mushrooms', 'extra cheese']
+if 'mushrooms' in r_top:
+    print("\nAdding mushrooms.")
+elif 'pepperoni' in r_top:
+    print("Adding pepperoni.")
+elif 'extra cheese' in r_top:
+    print("Adding extra cheese.")
+print("Finished making your pizza.")
+#Tests all conditions
+if 'mushrooms' in r_top:
+    print("\nAdding mushrooms.")
+if 'pepperoni' in r_top:
+    print("Adding pepperoni.")
+if 'extra cheese' in r_top:
+    print("Adding extra cheese.")
+print("Finished making your pizza.")
