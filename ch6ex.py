61ce2edd9b41c38e9547a4975a105f32a3775c90
Ray Rolison <rolisrgti06@gmail.com>
2020-08-11 21:37:16 -0500

    begin chapter 6

---
+++ ch6ex.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 20:58:36 2020
+
+@author: rdrolis
+"""
+
+#Exercise 6-1 Person
+person={
+        'last_name': 'rolison',
+        'first_name': 'daryl',
+        'age': 46,
+        'city': 'san antonio'
+        }
+print(person['last_name'].title())
+print(person['first_name'].title())
+print(person['age'])
+print(person['city'].title())
+print()
+
+#Exercise 6-2 Favorite Numbers
+fav_num= {
+    'joe': 6,
+    'dan': 23,
+    'bob': 5,
+    'jessica': 7,
+    'linda': 4,
+    }
+num = fav_num['joe']
+print(f"Joe's favorite number is {num}.")
+num = fav_num['dan']
+print(f"Dan's favorite number is {num}.")
+num = fav_num['bob']
+print(f"Bob's favorite number is {num}.")
+num = fav_num['jessica']
+print(f"Jessica's favorite number is {num}.")
+num = fav_num['linda']
+print(f"Linda's favorite number is {num}.\n")
+
+#Exercise 6-3 Glossary
+gloss = {
+    'list': 'A group of possible values for a variable.',
+    'variable': 'An item to be defined by a string or an integer.',
+    'integer': 'A whole number that can define a variable.',
+    'float': 'A number containing one or more decimal places.',
+    'conditional statement': 'A statement that defines conditions to be met \
+before executing a task',
+    }
+term = gloss['list']
+print(f"List: {term}")
+term = gloss['variable']
+print(f"Variable: {term}")
+term = gloss['integer']
+print(f"Integer: {term}")
+term = gloss['float']
+print(f"Float: {term}")
+term = gloss['conditional statement']
+print(f"COnditional Statement: {term}")
---
+++ examples/alien.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 20:25:14 2020
+
+@author: rdrolis
+"""
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+al_0 = {}
+al_0['color'] = 'green'
+al_0['points'] = 5
+al_0['x_pos'] = 0
+al_0['y_pos'] = 25
+print(al_0)
+al_0['color'] = 'yellow'
+al_0['speed'] = 'medium'
+
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed.
+if al_0['speed'] == 'slow':
+    x_increment = 1
+elif al_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+    
+#The new position is the old position plus the x increment
+al_0['x_pos'] = al_0['x_pos'] + x_increment
+print(f"New position: {al_0['x_pos']}")
+print(al_0)
+del al_0['points']
+print(al_0)
+#print(al_0['points'])
+point_value = al_0.get('points', 'No point value assigned.')
+print(point_value)
