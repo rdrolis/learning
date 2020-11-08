0182883429de1073d3d83bfc57c79d591750c5d2
Ray Rolison <rolisrgti06@gmail.com>
2020-11-08 05:28:58 -0600

    pizza order bug killing

--- onmyown/pizzaorder.py
+++ onmyown/pizzaorder.py
@@ -14,12 +14,12 @@ restart = True
 
 print("Welcome to Ray's Pizza Den. Please enter your order using our \
 automated system and we will be happy to serve you.")
-c = input("\nPlease give us your name for your order. \
+cu = input("\nPlease give us your name for your order. \
 Type 'q' at anytime to quit: ")
-if c == 'q':
+if cu == 'q':
     raise Exception("Thank you. Please visit us again soon.")
 else:
-    print(f"\nThank you {c.title()}. Please continue with your order: ")
+    print(f"\nThank you {cu.title()}. Please continue with your order: ")
 
 while restart:
     toppings = []
@@ -39,8 +39,7 @@ while restart:
         for to in toppings:
             print(f"-{to}")
 
-    pizza.append(c)
-    cu = pizza[0]
+    pizza.append(cu)
 
 # Find out pizza size
 
@@ -68,8 +67,8 @@ Please select from the available options.")
 
     print("\nWhat type of crust would you like? \
 You may choose from the following: ")
-    for c in cr:
-        print(f"-{c}")
+    for r in cr:
+        print(f"-{r}")
     while True:
         crust = input("Crust: ")
         if crust == 'q':
