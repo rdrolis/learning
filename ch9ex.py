19b6af6ba11b83bcd1e10dccd74098689d23b190
Ray Rolison <rolisrgti06@gmail.com>
2020-11-25 19:42:58 -0600

    start ch9 ex

--- ch9ex.py
+++ ch9ex.py
@@ -46,9 +46,33 @@ r4.open()
 
 class User:
     """ Define user information"""
-    def __init__(self, first, last, email, ):
+    def __init__(self, first, last, email, uname):
         """Initialize user attributes"""
         self.first = first
         self.last = last
+        self.email = email
+        self.uname = uname
+        
+    def info(self):
+        """Provide user info"""
+        print(f"\nLast Name: {self.last.title()}")
+        print(f"First Name: {self.first.title()}")
+        print(f"Username: {self.uname}")
+        print(f"Email address: {self.email}")
+    def greet(self):
+        print(f"\nHello {self.first.title()} {self.last.title()}, thank you \
+for logging in.")
+
+
+u1 = User('john', 'kelly', 'jkelly@cia.gov', 'jkelly')
+u2 = User('santa', 'clause', 'santa@northpole.com', 'sclause')
+u3 = User('jack', 'ryan', 'dci@cia.gov', 'jryan')
+
+u1.info()
+u1.greet()
+u2.info()
+u2.greet()
+u3.info()
+u3.greet()
+
     
-    def 
