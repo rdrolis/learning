57406b47252224d5159cb9073a474486cd0492d1
Ray Rolison <rolisrgti06@hotmail.com>
2021-01-24 21:29:16 -0600

    ch9 ex13

---
+++ ch9mod/ch9ex13.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 20:51:56 2021
+
+@author: rdrolis
+"""
+import random
+
+
+"""Module to simmulate rolling a die."""
+
+
+class Die:
+    """Model a die"""
+
+    def __init__(self, sides=6):
+        """Initialize a die that can be rolled"""
+        self.sides = sides
+
+    def roll_die(self):
+        """Return a number between 1 and number of sides"""
+        return random.randint(1, self.sides)
+
+
+# Roll the die 10 times and print the results
+d6 = Die()
+
+results = []
+
+for roll_num in range(10):
+    result = d6.roll_die()
+    results.append(result)
+
+print(results)
+
+d10 = Die(10)
+
+results = []
+
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+
+print(results)
+
+d20 = Die(20)
+
+results = []
+
+for roll_num in range(10):
+    result = d20.roll_die()
+    results.append(result)
+
+print(results)
